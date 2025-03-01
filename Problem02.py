9ba7c59ecf2461db1eb0f3b3fb4b50d40c278d5f
Kristina Cas <m.kristinacas@gmail.com>
2025-03-01 09:44:58 +0800

    Program02 Done

---
+++ Problem02.py
@@ -0,0 +1,9 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+num1 = input("First Number: ")
+num2 = input("Second Number: ")
+
+if num1 == num2:
+    print("Equal")
+else: 
+    print("Unequal")
