43722d399a3f66addf12e14de88252edb0d43444
Kristina Cas <m.kristinacas@gmail.com>
2025-03-01 09:39:58 +0800

    Program01 Done

---
+++ Problem01.py
@@ -0,0 +1,9 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+num1 = input("First Number: ")
+num2 = input("Second Number: ")
+
+if num1 > num2:
+    print(num1)
+else: 
+    print (num2)
