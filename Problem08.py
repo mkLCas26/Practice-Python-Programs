e98dec621a1ab65f4bbda4f5bd54fbdb8ec937e7
Kristina Cas <m.kristinacas@gmail.com>
2025-03-01 18:07:34 +0800

    Problem08 Done

---
+++ Problem08.py
@@ -0,0 +1,15 @@
+# Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+numbers = input("Enter 10 numbers (Use , and a space after to separate): ").split(", ")
+Num = []
+Odd = []
+
+
+for val in numbers:
+    Num.append(int(val))
+
+for num in Num:
+    if num % 2 != 0:
+        Odd.append(num)
+
+print(Odd)
